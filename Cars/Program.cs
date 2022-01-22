820e9e862fdfa6eaa307602d8695d4bdcad8721d
Süleyman Bayazit <suleymanbayazitt@gmail.com>
2022-01-22 15:08:38 +0300

    Add project files.

---
+++ Cars/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = ProcessFile("fuel.csv");
+            var query = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
+                .OrderByDescending(c => c.Combined)
+                .Select(car => car);
+
+            var result = cars.Any(c => c.Manufacturer == "Ford");
+            Console.WriteLine(result);
+            foreach (var car in query.Take(10))
+            {
+                Console.WriteLine($"{car.Manufacturer} {car.Name} : {car.Combined}");
+            }
+        }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            return
+                File.ReadAllLines(path)
+                    .Skip(1)
+                    .Where(line=> line.Length >1)
+                    .Select(Car.ParseFromCsv)
+                    .ToList();
+
+        }
+    }
+}
