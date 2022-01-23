2050bd07c497fbc74233dc7fe9b365908887f305
Süleyman Bayazit <suleymanbayazitt@gmail.com>
2022-01-23 20:39:28 +0300

    LINQ with Entity Framework new queries added

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Entity;
 
 namespace Cars
 {
@@ -6,45 +7,117 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-            var cars = ProcessCars("fuel.csv");
-            var manufacturers = ProcessManufacturers("manufacturers.csv");
             {
-                //var query = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
-                //    .OrderByDescending(c => c.Combined)
-                //    .Select(car => car);
-
-                //var result = cars.Any(c => c.Manufacturer == "Ford");
-                //Console.WriteLine(result);
-
-                //var query = 
-                //    cars.Join(manufacturers,
-                //            c => new { c.Manufacturer, c.Year },
-                //            m => new { Manufacturer =  m.Name, m.Year}, 
-                //            (c, m) => new
-                //            {
-                //                m.Headquarters,
-                //                c.Name,
-                //                c.Combined
-                //            })
-                //    .OrderByDescending(c => c.Combined)
-                //    .ThenBy(c => c.Name);
-
-                //foreach (var car in query.Take(10))
-                //{
-                //    Console.WriteLine($"{car.Headquarters} {car.Name} : {car.Combined}");
-                //}
-            }
-
-            var query = cars.GroupBy(c => c.Manufacturer.ToUpper())
-                            .OrderBy(g => g.Key);
-
-            foreach (var group in query)
-            {
-                Console.WriteLine(group.Key);
-                foreach (var item in group.OrderByDescending(c=>c.Combined).Take(2))
                 {
-                    Console.WriteLine($"\t{item.Name}  :  {item.Combined}");
+                    //var cars = ProcessCars("fuel.csv");
+                    //var manufacturers = ProcessManufacturers("manufacturers.csv");
                 }
+                {
+
+
+                    //var query = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
+                    //    .OrderByDescending(c => c.Combined)
+                    //    .Select(car => car);
+
+                    //var result = cars.Any(c => c.Manufacturer == "Ford");
+                    //Console.WriteLine(result);
+
+                    //var query = 
+                    //    cars.Join(manufacturers,
+                    //            c => new { c.Manufacturer, c.Year },
+                    //            m => new { Manufacturer =  m.Name, m.Year}, 
+                    //            (c, m) => new
+                    //            {
+                    //                m.Headquarters,
+                    //                c.Name,
+                    //                c.Combined
+                    //            })
+                    //    .OrderByDescending(c => c.Combined)
+                    //    .ThenBy(c => c.Name);
+
+                    //foreach (var car in query.Take(10))
+                    //{
+                    //    Console.WriteLine($"{car.Headquarters} {car.Name} : {car.Combined}");
+                    //}
+                }
+                {
+                    //var query = cars.GroupBy(c => c.Manufacturer.ToUpper())
+                    //                .OrderBy(g => g.Key);
+                }
+                {
+                    //var query = manufacturers.GroupJoin(
+                    //cars,
+                    //m => m.Name,
+                    //c => c.Manufacturer,
+                    //(m, g) =>
+                    //new
+                    //{
+                    //    Manufacturer = m,
+                    //    Cars = g
+                    //})
+                    //.OrderBy(m => m.Manufacturer.Name);
+                }
+                {
+                    //var query = manufacturers.GroupJoin(
+                    //    cars,
+                    //    m => m.Name,
+                    //    c => c.Manufacturer,
+                    //    (m, g) =>
+                    //    new
+                    //    {
+                    //        Manufacturer = m,
+                    //        Cars = g
+                    //    })
+                    //    .GroupBy(m => m.Manufacturer.Headquarters);
+                }
+                {
+                    //foreach (var group in query)
+                    //{
+                    //    Console.WriteLine($"{group.Manufacturer.Name}:{group.Manufacturer.Headquarters}");
+                    //    foreach (var item in group.Cars.OrderByDescending(c=>c.Combined).Take(2))
+                    //    {
+                    //        Console.WriteLine($"\t{item.Name}  :  {item.Combined}");
+                    //    }
+                    //}
+                }
+                {
+                    //foreach (var result in query)
+                    //{
+                    //    Console.WriteLine($"{result.Name}");
+                    //    Console.WriteLine($"\t Max: {result.Max}");
+                    //}
+                }
+            }
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<CarDb>());
+            InsertData();
+            QueryData();
+
+        }
+
+        private static void QueryData()
+        {
+            var db = new CarDb();
+            db.Database.Log = Console.WriteLine;
+            var query = db.Cars
+                .OrderByDescending(x => x.Combined)
+                .ThenBy(x => x.Name);
+            foreach (var item in query.Take(10))
+            {
+                Console.WriteLine($"{item.Name} : {item.Combined}");
+            }
+        }
+
+        private static void InsertData()
+        {
+            var cars = ProcessCars("fuel.csv");
+            var db = new CarDb();
+            if (!db.Cars.Any())
+            {
+                foreach (var car in cars)
+                {
+                    db.Cars.Add(car);
+                }
+                db.SaveChanges();
             }
         }
 
